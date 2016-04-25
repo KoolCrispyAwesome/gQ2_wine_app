487d1e2e485034a928d47b273908912abce644a7
Andrew Nguyen <andrew.nguyen2118@gmail.com>
2016-04-25 14:08:34 -0700

    adding users views files

--- db/migrations/20160423154652_create_users_wines_mains_sides.js
+++ db/migrations/20160423154652_create_users_wines_mains_sides.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users_favs', t => {
+    t.increments();
+    t.text('user_id');
+    t.integer('wine_id');
+    t.integer('mains_id');
+    t.integer('sides_id');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users_favs');
 };
