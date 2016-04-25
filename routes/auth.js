248538cfb3ae16a8608cc40455564fcabd4f051c
Kyle Malfatti <kyle.malfatti@gmail.com>
2016-04-25 14:56:39 -0700

    fixed routes auth typo

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,7 @@ router.get('/facebook', passport.authenticate('facebook', {scope: ['email']}));
 router.get('/facebook/callback', passport.authenticate('facebook', {
   failureRedirect: '/auth/login'
 }), (req, res) => {
-  res.redireect('/');
+  res.redirect('/');
 });
 
 router.get('/logout', (req, res) => {
