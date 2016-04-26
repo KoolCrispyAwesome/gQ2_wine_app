9195ad3d2c8b34fa7445a831bf0bf759d020c064
Kyle Malfatti <kyle.malfatti@gmail.com>
2016-04-26 10:23:26 -0700

    fix matching typo

--- helpers/matching.js
+++ helpers/matching.js
@@ -10,7 +10,7 @@ var dessert = ["sorbet", "vanilla pudding", "apple pie", "creme brulee", "cake",
 
 var sauvignonBlanc = 0;
 var chardonnay = 0;
-var rlesling = 0;
+var riesling = 0;
 var pinotNoir = 0;
 var syrah = 0;
 var merlot = 0;
@@ -18,7 +18,7 @@ var cabernetSanvignon = 0;
 var zinfandel = 0;
 if (v === "asparagus") {sauvignonBlanc += .2}
   else if (v === "squash") {chardonnay += .2}
-  else if (v === "chilli peppers") {rlesling += .2} 
+  else if (v === "chilli peppers") {riesling += .2} 
   else if (v === "mushrooms") {pinotNoir += .2}
   else if (v === "tomatoes") {syrah += .2}
   else if (v === "onions") {merlot += .2}
@@ -27,8 +27,8 @@ if (v === "asparagus") {sauvignonBlanc += .2}
 
 if (c === "feta") {sauvignonBlanc += .2}
   else if (c === "asiago") {chardonnay += .2}
-  else if (c === "havarti") {chardonnay += .2; rlesling += .2}
-  else if (c === "gouda") {rlesling += .2}
+  else if (c === "havarti") {chardonnay += .2; riesling += .2}
+  else if (c === "gouda") {riesling += .2}
   else if (c === "goat cheese") {pinotNoir += .2}
   else if (c === "cheddar") {syrah += .2; cabernetSanvignon += .2}
   else if (c === "parmesan") {merlot += .2}
@@ -39,7 +39,7 @@ if (c === "feta") {sauvignonBlanc += .2}
 if (m === "chicken") {sauvignonBlanc += .45; chardonnay += 45}
   else if (m === "turkey") {sauvignonBlanc += .45}
   else if (m === "veal") {chardonnay += .45}
-  else if (m === "duck") {rlesling += .45; zinfandel += .45}
+  else if (m === "duck") {riesling += .45; zinfandel += .45}
   else if (m === "lamb") {pinotNoir += .45}
   else if (m === "filet mignon") {pinotNoir +=.45}
   else if (m === "pepperoni") {syrah += .45}
@@ -51,7 +51,7 @@ if (m === "chicken") {sauvignonBlanc += .45; chardonnay += 45}
   else if (m === "scallops") {sauvignonBlanc += .45}
   else if (m === "shrimp") {chardonnay += .45}
   else if (m === "crab") {chardonnay += .45}
-  else if (m === "trout") {rlesling += .45}
+  else if (m === "trout") {riesling += .45}
   else if (m === "tuna") {pinotNoir += .45; merlot += .45; cabernetSanvignon += .45}
   else if (m === "salmon") {syrah += .45}
   else if (m === "swordfish") {merlot += .45}
@@ -59,18 +59,18 @@ if (m === "chicken") {sauvignonBlanc += .45; chardonnay += 45}
 
 if (d === "sorbet") {sauvignonBlanc += .15}
   else if (d === "vanilla pudding") {chardonnay += .15}
-  else if (d === "apple pie") {rlesling += .15}
+  else if (d === "apple pie") {riesling += .15}
   else if (d === "creme brulee") {pinotNoir += .15}
   else if (d === "cake") {syrah += .15}
   else if (d === "dark chocolate") {merlot += .15}
   else if (d === "bittersweet") {cabernetSanvignon += .15}
   else if (d === "carrot cake") {zinfandel += .15}
 
-var max = Math.max(sauvignonBlanc, chardonnay, rlesling, pinotNoir, syrah, merlot, cabernetSanvignon, zinfandel)
+var max = Math.max(sauvignonBlanc, chardonnay, riesling, pinotNoir, syrah, merlot, cabernetSanvignon, zinfandel)
 
 if (max === sauvignonBlanc) {return "Sauvignon Blanc"}
   else if (max === chardonnay) {return "Chardonnay"}
-  else if (max === rlesling) {return "Rlesling"}
+  else if (max === riesling) {return "Riesling"}
   else if (max === pinotNoir) {return "Pinot Noir"}
   else if (max === syrah) {return "Syrah"}
   else if (max === merlot) {return "Merlot"}
