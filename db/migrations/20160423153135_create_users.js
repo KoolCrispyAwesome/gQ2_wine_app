c0bd4de8bb4e96f15ca2a2e5b00f500b5831312f
Christina Jensen <cj.christina.jensen@gmail.com>
2016-04-27 13:37:44 -0700

    adding new migrations

--- db/migrations/20160423153135_create_users.js
+++ db/migrations/20160423153135_create_users.js
@@ -6,6 +6,7 @@ exports.up = function(knex, Promise) {
     t.text('last_name');
     t.text('password');
     t.text('facebook_id');
+    t.text('photo').defaultTo('http://dev.bukkit.org/thumbman/avatars/14/772/160x166/man_wearing_hat_silhouette.png.-m0.png');
   });
 };
 
